1fa828309c6f472d443e0d2b9e540535ca7e3085
shintarou.chou <shintarou.chou@gmail.com>
2022-12-14 10:19:38 +0900

    配置webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
